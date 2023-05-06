be818990ac83fe21a6a831917361e56a5d0760db
Shivam Gupta <shivam.gupta1@remotestate.com>
2023-05-06 15:16:48 +0530

    added map

---
+++ src/components/Blinking.js
@@ -0,0 +1,8 @@
+import "./Blinking.css"
+
+const Blinking =()=>{
+    return <img class="blink" src = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRs77forVmh5mEvdMH6zZmHqXaXqs6MkSs8ZOf1HrblLw&usqp=CAU&ec=48665701"/>
+  
+
+}
+export default Blinking;
